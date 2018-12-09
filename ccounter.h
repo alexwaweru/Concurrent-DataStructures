05500368b63cf8e453e1378b8ea0d76d822bd0fd
Alex Waweru <njorogealexw@gmail.com>
2018-12-09 10:25:21 -0800

    working counter

--- ccounter.h
+++ ccounter.h
@@ -6,22 +6,32 @@
 #include <unistd.h>
  typedef struct _counter_t{
 	int value;
+	pthread_mutex_t lock;
 }counter_t;
  void init(counter_t *c) {
-	//Pthread_mutex_lock(&c->lock);
+	
 	c->value = 0;
-	//Pthread_mutex_unlock(&c->lock);
+	pthread_mutex_init(&c->lock, NULL);
+	
 }
  void increment(counter_t *c){
+	pthread_mutex_lock(&c->lock);
 	c->value++;
+	pthread_mutex_unlock(&c->lock);
 }
  void decrement(counter_t *c){
+	pthread_mutex_lock(&c->lock);
 	c->value--;
+	pthread_mutex_unlock(&c->lock);
 }
  int get(counter_t *c){
-	return c->value;
+	pthread_mutex_lock(&c->lock);
+	int rc = c->value;
+	pthread_mutex_lock(&c->lock);
+	return rc;
 }
- counter_t mycounter;
+ 
+counter_t mycounter;
 void *myThread(void *vargp){
 	printf("%s\n", "thread is created");
 	increment(&mycounter);
--- counter-test.c
+++ counter-test.c
@@ -2,9 +2,58 @@
 
 #include "ccounter.h"
 
+/*************************************
+void counter1(int argc, char const *argv[])
+{
+    printf("Thread 1\n");
+    struct _counter_t c;
+
+    init(counter_t *c)
+    int t = 11;;
+    int r = 10;
+    int s = t+r;
+    if(s >0){
+	 increment(&c);
+    }
+    return;
+}
+void counter2(int argc, char const *argv[])
+{
+    printf("Thread 1\n");
+    struct _counter_t c;
+
+    init(counter_t *c)
+    int t = 10;;
+    int r = 10;
+    int s = t-r;
+    if(s < 1 ){
+	 increment(&c);
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t p1, p2;
+    int rc;
+    printf("main: begin\n");
+    pthread_create(&p1, NULL, counter1, "A");
+    pthread_create(&p2, NULL, counter2, "B");
+
+    // join waits for the threads to finish
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    printf("%s%d\n", "the counter is at ", get(&c));
+    return 0;
+
+}
+
+*********************************************************/
+
+
 int main(int argc, char const *argv[])
 {
-	int threads = 10;
+	int threads = 2;
 	if (argc > 1)
 	{
 		threads = atoi(argv[1]);
@@ -21,11 +70,12 @@ int main(int argc, char const *argv[])
 		pthread_create(&thread_id, NULL, myThread, (void *)&thread_id);
 	}
 	int end_time = gettimeofday(&tv2, NULL);
-	printf("%s\n", "after creating threads" );
+	printf("%s\n", "after creating thread" );
  	long int difference = tv2.tv_usec - tv1.tv_usec;
- 	printf("%s%ld\n", "time took to incrument is: ", difference);
+ 	printf("%s%ld\n", "time taken to increment is: ", difference);
 	printf("%s%d\n", "the counter is at ", get(&mycounter) );
 	
  	
 	return 0;
 } 
+
